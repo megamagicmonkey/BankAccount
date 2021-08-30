aa5e8da1973ddd0a1b82573028b95ab3eeecc1d9
megamagicmonkey <megamagicmonkey@gmail.com>
2021-08-30 12:16:29 -0500

    Initial commit

---
+++ BankAccount/BankAccount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    class BankAccount
+    {
+        private double balance = 0;
+
+
+        public void Deposit(double num)
+        {
+            balance += num;
+        }
+
+        public void GetBalance()
+        {
+            Console.WriteLine($"${balance}");
+        }
+    }
+}
---
+++ BankAccount/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BankAccount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount myAcct = new BankAccount();
+
+            Console.WriteLine("Welcome to Minimal Banking banking services! We see you have an account! Would you like to make a deposit?");
+            Console.WriteLine("Of course you do! It's our primary and singular service!");
+            Console.WriteLine("Please input the amount to deposit:");
+            myAcct.Deposit (Convert.ToDouble(Console.ReadLine()));
+            Console.WriteLine($"After your deposit, which is in no way having security checks placed on the transaction, your account now holds:");
+            myAcct.GetBalance();
+        }
+    }
+}
